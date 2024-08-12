263f4fe70d3e544e142be86c06326e2c346fc831
deffstudio <dudeyahuu@gmail.com>
2024-08-12 16:35:19 +0700

    Updated sub menu

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,35 @@ fn main() {
 
         match choice.trim() {
             "1" => println!(" 📊 Bot Status: Active"),
-            "2" => println!("Starting Trading..."),
+            "2" => start_trading(),
             "3" => println!(" 🛑 Stopping Trading..."),
             "4" => {
                 println!(" ❌ Exiting...");
                 break;
             }
             _ => println!("Invalid choice. Please try again."),
+        }
+    }
+}
+
+fn start_trading() {
+    loop {
+        println!("📈 Select Trading Strategy:");
+        println!("1. 🥇 Strategy A");
+        println!("2. 🥈 Strategy B");
+        println!("3. 🔙 Back to Main Menu");
+
+        let mut choice = String::new();
+
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Failed to read input");
+
+        match choice.trim() {
+            "1" => println!("Starting 🥇 Strategy A..."),
+            "2" => println!("Starting 🥈 Strategy B..."),
+            "3" => break,
+            _ => println!("⚠️ Invalid choice. Please try again."),
         }
     }
 }
